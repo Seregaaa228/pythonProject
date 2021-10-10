4c0b97f673bf9892e30c528bda14649e137a06b2
skeka <shreker240@gmail.com>
2021-10-10 18:53:33 +0300

    rework

--- main.py
+++ main.py
@@ -4,37 +4,39 @@ import string
 letters = string.ascii_letters
 
 
+class Car(object):
+    def __init__(self, name: str, vin_number: str, model: str):
+        self.name = name
+        self.vin_number = vin_number
+        self.model = model
+
+    def __str__(self):
+        return "Car(name=" + self.name + ', vin_number=' + self.vin_number + ", model=" + self.model + ")"
+
+    def __repr__(self):
+        return self.name
+
+    @classmethod
+    def create_random(cls):
+        return cls(
+            name="".join(random.choice(letters) for _ in range(10)),
+            vin_number="".join(random.choice(letters) for _ in range(5)),
+            model="".join(random.choice(letters) for _ in range(10)),
+        )
+
+
+class Human(object):
+    def __init__(self, name: str, age: int, current_car: Car, cars: list):
+        self.cars = cars
+        self.current_car = current_car
+        self.age = age
+        self.name = name
+
+    def __str__(self):
+        return f"Его зовут {self.name} - {self.age} возраст, он катается на {self.current_car}, Гараж из {len(self.cars)} машин(ы) "
+
+
 def main() -> None:
-    class Car(object):
-        def __init__(self, name: str, vin_number: str, model: str):
-            self.name = name
-            self.vin_number = vin_number
-            self.model = model
-
-        def __str__(self):
-            return "Car(name=" + self.name + ', vin_number=' + self.vin_number + ", model=" + self.model + ")"
-
-        def __repr__(self):
-            return self.name
-
-        @classmethod
-        def create_random(cls):
-            return cls(
-                name="".join(random.choice(letters) for _ in range(10)),
-                vin_number="".join(random.choice(letters) for _ in range(5)),
-                model="".join(random.choice(letters) for _ in range(10)),
-            )
-
-    class Human(object):
-        def __init__(self, name: str, age: int, current_car: Car, cars: list):
-            self.cars = cars
-            self.current_car = current_car
-            self.age = age
-            self.name = name
-
-        def __str__(self):
-            return f"Его зовут {self.name} - {self.age} возраст, он катается на {self.current_car}, Гараж из {len(self.cars)} машин(ы) "
-
     quantiny1 = random.randint(1, 8)
     quantiny2 = 9 - quantiny1
     garage1 = [Car.create_random() for _ in range(quantiny1)]
