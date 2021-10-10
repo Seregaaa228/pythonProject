284a38b73e320801a6907ee4c54964fdc5d5b014
skeka <shreker240@gmail.com>
2021-10-10 19:59:03 +0300

    rework4

--- main.py
+++ main.py
@@ -36,29 +36,30 @@ class Human(object):
         return f"Его зовут {self.name} - {self.age} возраст, он катается на {self.current_car}, Гараж из {len(self.cars)} машин(ы) "
 
 
-quantiny1 = random.randint(1, 8)
-quantiny2 = 9 - quantiny1
-garage1 = [Car.create_random() for _ in range(quantiny1)]
-garage2 = [Car.create_random() for _ in range(quantiny2)]
-
-human1 = Human(name="Valera", age=54, current_car=random.choice(garage1), cars=garage1)
-human2 = Human(name="Denis", age=27, current_car=random.choice(garage2), cars=garage2)
-
-
 def main() -> None:
-    choice = input(f"1 - {human1.name} , 2 - {human2.name} выберите номер - ")
+    quantiny1 = random.randint(1, 8)
+    quantiny2 = 9 - quantiny1
+    garage1 = [Car.create_random() for _ in range(quantiny1)]
+    garage2 = [Car.create_random() for _ in range(quantiny2)]
 
-    match int(choice):
-        case 1:
-            choiceCar = input(f"на какой машинке поедет {human1.name} выберите по номеру от 0 до {quantiny1} - ")
-            human1.current_car = garage1[int(choiceCar)]
-            print(f'{human1.name} поедет на машине {human1.current_car}')
-        case 2:
-            choiceCar = input(f"на какой машинке поедет {human2.name} выберите по номеру от 0 до {quantiny2} - ")
-            human2.current_car = garage2[int(choiceCar)]
-            print(f'{human2.name} поедет на машине {human2.current_car}')
-        case _:
-            print("Ошибка нету такого ")
+    human1 = Human(name="Valera", age=54, current_car=random.choice(garage1), cars=garage1)
+    human2 = Human(name="Denis", age=27, current_car=random.choice(garage2), cars=garage2)
+    while True:
+        choice = input(f"1 - {human1.name} , 2 - {human2.name} выберите номер - ")
+
+        match int(choice):
+            case 1:
+                choiceCar = input(f"на какой машинке поедет {human1.name} выберите по номеру от 0 до {quantiny1} - ")
+                human1.current_car = garage1[int(choiceCar)]
+                print(f'{human1.name} поедет на машине {human1.current_car}')
+                break
+            case 2:
+                choiceCar = input(f"на какой машинке поедет {human2.name} выберите по номеру от 0 до {quantiny2} - ")
+                human2.current_car = garage2[int(choiceCar)]
+                print(f'{human2.name} поедет на машине {human2.current_car}')
+                break
+            case _:
+                print("Ошибка нету такого ")
 
 
 if __name__ == "__main__":
